57ddb711e2660a1d2c028fa372b9ce5006827311
naime <naimeh461@gmail.com>
2025-10-19 01:24:05 +0600

    update role base

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY)
+const jwt = require('jsonwebtoken');
 app.use(cors())
 app.use(express.json())
 const graduate = require("./data/college.json")
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'no authorization header is send' })
+  }
+  // if token  in send in headers.authorization the splite it by bearer token
+  const token = authorization.split(' ')[1];
 
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: 'token is not valid' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tktqebe.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,6 +48,38 @@ async function run() {
     const university = client.db("admitHub").collection("universities");
     const userCollection = client.db("admitHub").collection("users");
     const reviewsCollection = client.db("admitHub").collection("reviews");
+
+
+    
+    //  ----------------------
+    //  jwt related api 
+    //  ---------------------
+    // send the jwt token when user log in or register
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
+      res.send({ token });
+    })
+
+     const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next()
+    }
+
+     const verifyStudent = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      if (user?.role !== "student") {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next()
+    }
 
     app.get("/university", async (req, res) => {
       const search = req.query.search;
